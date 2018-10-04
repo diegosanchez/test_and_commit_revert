d5d31f9eeb53f98820eeadf4ef50d6cd5b1c814b
diegosanchez <diego.sanchez@mercadolibre.com>
2018-10-04 16:12:12 -0300

    working

--- test/solution.js
+++ test/solution.js
@@ -1,21 +1,7 @@
 const expect = require('chai').expect;
 
-const BillingInfo = require('../src/billing_info');
-const Card = require('../src/card');
-const UserIdentificationContext = require('../src/user_identification_context');
-const UserIdentificationLoader = require('../src/user_identification_loader');
 
-describe("billing info tests", () => {
+describe("Dojo", () => {
 
-    it('should given an user with card should preload bi with card data', () => {
-
-        const card = new Card("DNI", "34842320");
-        const userIdentificationContext = new UserIdentificationContext("MLA", card, null, false);
-
-        const userIdentificationLoader = new UserIdentificationLoader();
-        const billingInfo = userIdentificationLoader.preloadUserIdentificationData(userIdentificationContext);
-        const expectedBillingInfo = new BillingInfo("DNI", "34842320");
-
-        expect(expectedBillingInfo).to.deep.equal(billingInfo);
-    });
+    it('X', () => {});
 });

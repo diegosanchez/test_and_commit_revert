526e12c165bc49786b04f2ca323f52951fe32004
diegosanchez <diego.sanchez@mercadolibre.com>
2018-10-07 08:20:12 -0300

    working

--- commit_forever.sh
+++ commit_forever.sh
@@ -2,7 +2,7 @@
 
 while(true); do
     n=5;
-    npm test && git commit -am working
+    npm test && git add . && git commit -am working
     echo "Commiter - `date` - Sleeping $n..."
     sleep $n;
 done
